3b29956eb7a4484549d32dd057444cac7227b2a4
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:41 -0300

    Arraia bidimencional

---
+++ ConsoleArrayBi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArrayBi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listadelivros = new string[2, 2]
+                {{"O pequeno","sim"},{"O grande","não"} };
+
+            for (int i = 0; i < listadelivros.GetLength(0); i++)
+            {
+                var Nomelivro = listadelivros[i, 0];
+                var disponivel = listadelivros[i, 1];
+
+                Console.WriteLine($"Nomedo livro:{Nomelivro} disponivel:{disponivel}.");
+            }
+
+            Console.ReadKey();
+
+
+            
+
+        }
+    }
+}
