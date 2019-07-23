e03a27700451f3a70e08e79477c08c6b595d78b9
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 17:26:37 -0300

    class get/set

---
+++ MeuCarro4x4Class/MyFirstClass/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCarro4x4Class.MyFirstClass
+{
+    class Carro
+    {
+        public int rodas { get; set; } = 0;
+        public int portas { get; set; } = 0;
+        public int bancos { get; set; } = 0;
+        public int janelas { get; set; } = 0; //se não especificar public fica privado
+        
+        public void MostrarCarro()
+        {
+            Console.WriteLine($"Nosso carro:\r\n" +
+                $"Quant Rodas[{rodas}]\r\n" +
+                $"Quant Portas[{portas}]\r\n" +
+                $"Quant Bancos[{bancos}]\r\n" +
+                $"Quant Janelas[{janelas}]");
+        }
+    }
+}
