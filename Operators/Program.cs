6ce8b6abd1cf141d7bd5b4719bcc1e3470345440
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 16:58:19 -0300

    Class

---
+++ Operators/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elementoX = new Teste(5,5);
+
+            elementoX.MostrarInformaçoes();
+
+            elementoX.AlteraInformaçoes(5, 5);
+
+            elementoX.MostrarInformaçoes();
+
+            elementoX.AlteraInformaçoes();
+
+            elementoX.MostrarInformaçoes();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Teste
+    {
+        private int A { get; set; } = 0;
+        private int B { get; set; } = 0;
+        public Teste(int pA, int pB)
+        {
+            A = pA; 
+            B = pB;
+        }
+
+        public void AlteraInformaçoes()
+        {
+            A = B * 100;
+        }
+        public void AlteraInformaçoes(int pA, int pB)
+        {
+            A = pA * 2;
+            B = pB * 2;
+        }
+        public void MostrarInformaçoes()
+        {
+            Console.WriteLine(A + B);
+        }
+    }
+}
