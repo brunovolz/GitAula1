ff00a71cc3037dce79960e28117586695ac5ca32
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 11:03:47 -0300

    Classe criada

---
+++ CriandoListas/NewFolder1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoListas.NewFolder1
+{
+    class Class1
+    {
+        string[] Lista;
+
+        public Class1()
+        {
+            Lista = new string[10];
+        }
+        public void CarregaLista()
+        {
+            for (int i = 0; i < Lista.GetLength(0); i++)
+            {
+                Lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss:fff");
+            }
+            foreach (var item in Lista)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+        public void LimpaLista()
+        {
+            for (int i = 0; i < Lista.Length; i++)
+            {
+                Lista[i] = string.Empty;
+            }
+            foreach (var item in Lista)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+        
--- CriandoListas/Program.cs
+++ CriandoListas/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CriandoListas.NewFolder1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,37 +11,12 @@ namespace CriandoListas
     {
         static void Main(string[] args)
         {
-            //iniciando um lista de string com array linear
-            string[] lista = new string[10];//aqui definimos as posições da lista iniciando ela, 
-            //com 10 espaços na memoria para alocar informações no texto.
+            var Lista = new Class1();
 
-            //laço que usamos para andar sobre as posições da lista.
-            for (int i = 0; i < lista.Length; i++)
-            {
-                //carregando os valores da nossa lista aqui
-                //onde "i" representa cada espaço que temos disponivel.
-                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss:fff");
-                //DateTime é um tipo do .net c# que disponibiliza funções relacionadas a datas
-                //e horas, ou seja, se precisar trabalhar com essas informaçoes é possivel usar ele.
-            }
-            //Laço de repeti~ção que usamos para varrer nossa lista de maneira mais simples.
-            foreach (var item in lista) //"var item" indica uma unidade da nossa lista "in lista"
-                //indica a lista que desejamos varrer
-                Console.WriteLine(item);//Aqui apresentamos a informação na tela.
+            Lista.CarregaLista();
+            Lista.LimpaLista();
 
             Console.ReadKey();
-
-            for (int i = 0; i < lista.Length; i++)
-            {
-                lista[i] = string.Empty; //Aqui limpamos o nosso valor dentro da coleção;
-            }
-            //Laço de repeti~ção que usamos para varrer nossa lista de maneira mais simples.
-            foreach (var item in lista) //"var item" indica uma unidade da nossa lista "in lista"
-                //indica a lista que desejamos varrer
-                Console.WriteLine(item);//Aqui apresentamos a informação na tela.
-
-            Console.ReadKey();
-
         }
     }
 }
