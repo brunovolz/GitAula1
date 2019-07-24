ebeb33569f1b42ceb0eb110b2487676708afd21c
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:15:51 -0300

    Calculadora

---
+++ BibliotecaCalculadora/Biblioteca.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCalculadora
+{
+    public class Biblioteca
+    {
+        public void Divisao()
+        {
+            Console.WriteLine("Digite um número");
+            float.TryParse(Console.ReadLine(), out float x);
+            Console.WriteLine("Digite outro número para dividir");
+            float.TryParse(Console.ReadLine(), out float i);
+
+            Console.WriteLine($"{x /= i}");
+
+            Console.ReadKey();
+        }
+        public void Subtracao()
+        {
+            Console.WriteLine("Digite um número");
+            float.TryParse(Console.ReadLine(), out float x);
+            Console.WriteLine("Digite outro número para subtrair");
+            float.TryParse(Console.ReadLine(), out float i);
+
+            Console.WriteLine($"{x -= i}");
+
+            Console.ReadKey();
+        }
+        public void Adicao()
+        {
+            Console.WriteLine("Digite um número");
+            float.TryParse(Console.ReadLine(), out float x);
+            Console.WriteLine("Digite outro número para somar");
+            float.TryParse(Console.ReadLine(), out float i);
+
+            Console.WriteLine($"{x += i}");
+
+            Console.ReadKey();
+        }
+        public void Multiplicacao()
+        {
+            Console.WriteLine("Digite um número");
+            float.TryParse(Console.ReadLine(), out float x);
+            Console.WriteLine("Digite outro número para multiplicar");
+            float.TryParse(Console.ReadLine(), out float i);
+
+            Console.WriteLine($"{x *= i}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using BibliotecaCalculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Calculadora = new Biblioteca();
+
+            Calculadora.Divisao();
+            Calculadora.Subtracao();
+            Calculadora.Multiplicacao();
+            Calculadora.Adicao();
+
+            Console.ReadKey();
+        }
+    }
+}
