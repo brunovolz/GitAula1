d110b54781d145747836ce6038e5c45c2f383c50
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 13:40:08 -0300

    Cantina

---
+++ Cantina/Lanche/Lancheria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Lanche
+{
+    public class Lancheria
+    {
+        double saldo = 20;
+        public Lancheria()
+        {
+            saldo = 20;
+        }
+        public bool ComprarLanche(string pLanche)
+        {
+            double Coxinha = 2;
+            double Pastel = 3;
+            if (pLanche == "coxinha" && Coxinha <= saldo)
+            {
+                saldo -= Coxinha;
+                return true;
+            }
+            if (pLanche == "pastel" && Pastel <= saldo)
+            {
+                saldo -= Pastel;
+                 return true;
+            }
+            return false;
+        }
+        public double MostrarSaldo()
+        {
+            return saldo;
+        }
+    }
+}
---
+++ Cantina/Program.cs
@@ -0,0 +1,45 @@
+﻿using Cantina.Lanche;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina
+{
+    class Program
+    {
+        Lancheria ComprarLanchinho = new Lancheria();
+        static void Main(string[] args)
+        {
+            EfetuarCompra();
+
+        }
+        public static void EfetuarCompra()
+        {
+            Lancheria ComprarLanchinho = new Lancheria();
+
+            Console.WriteLine("Informe o lanche a ser comprado\r\n" +
+            "Coxinha = 2,00 reais ou Pastel = 3,00 reais");
+
+            string lanche = Console.ReadLine().ToLower();
+
+            if (lanche != "" && ComprarLanchinho.ComprarLanche(lanche))
+                Console.WriteLine("Lanchinho comprado com susssscesso!");
+            else
+                Console.WriteLine("Você não tem dinheiro para comprar o lanchinho :(");
+
+            Console.WriteLine($"Saldo atual disponiVel:{ComprarLanchinho.MostrarSaldo()}\r\n");
+            Console.WriteLine("(1) Desejo comprar outro lanchinho\r\n" +
+                "(2) Desejo sair da cantina");
+            var opcao = Console.ReadLine();
+
+            if (opcao == "1")
+            {
+                Console.Clear();
+                EfetuarCompra();
+            }
+        }
+        
+    }
+}
